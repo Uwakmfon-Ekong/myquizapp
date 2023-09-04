69a1b70c4d499d15967bc194c403fc5b309234ad
Uwakmfon-Ekong <uwakmfone470@gmail.com>
2023-09-04 13:03:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const questions = [
+    {
+        question: "Which is the largest animal in the world?",
+        answers: [
+            { text: "monkey", correct: false },
+            { text: "Blue whale", correct: true},
+            { text: "Shark", correct: false },
+            { text: "Goat", correct: false }
+        ]
+
+    }
+]
